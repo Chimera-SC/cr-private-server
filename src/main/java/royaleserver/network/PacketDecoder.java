b7f1c4957fa1b699c5c8691cac8c3b5ff68d4fce
Chimera <pixelmcpe1@gmail.com>
2026-07-21 20:28:20 +0800

    fix keepalive

--- src/main/java/royaleserver/network/PacketDecoder.java
+++ src/main/java/royaleserver/network/PacketDecoder.java
@@ -30,70 +30,86 @@ public class PacketDecoder extends ByteToMessageDecoder {
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		if (!hasHeader) {
-			if (in.readableBytes() >= 7) {
-				short id = in.readShort();
-				int payloadLength = in.readMedium();
-				short version = in.readShort();
+			if (in.readableBytes() < 7) return;
 
-				hasHeader = true;
-				header.id = id;
-				header.payload = new byte[payloadLength];
-			}
-		} else {
-			if (in.readableBytes() >= header.payload.length) {
-				in.readBytes(header.payload);
+			short id = in.readShort();
+			int payloadLength = in.readMedium();
+			short version = in.readShort();
+
+			header.id = id;
+			header.payload = new byte[payloadLength];
+
+			// if there is empty payload packets then handle it immediately
+			if (payloadLength == 0) {
 				hasHeader = false;
-
-				crypto.decryptPacket(header);
-
-				ClientMessage message = processPacket();
-				if (message != null) {
-					out.add(message);
+				if (header.id == 10108) {
+					header.decrypted = new byte[0];
+				} else {
+					crypto.decryptPacket(header);
 				}
+				ClientMessage message = processPacket();
+				if (message != null) out.add(message);
+				return;
 			}
-		}
-	}
-
-	private ClientMessage processPacket() {
-		ClientMessage message = ClientMessageFactory.instance.create(header.id);
-
-		if (header.decrypted == null) {
-			logger.error("Failed to decrypt packet %d, encrypted payload:\n%s", header.id, Hex.dump(header.payload));
-			return null;
+			hasHeader = true;
 		}
 
-		if (message == null) {
-			String name = null;
-			if (Messages.messagesMap.containsKey(header.id)) {
-				name = Messages.messagesMap.get(header.id);
-			}
-
-			if (name == null) {
-				logger.warn("Received unknown packet %d:\n%s", header.id, Hex.dump(header.decrypted));
+		if (hasHeader) {
+			if (in.readableBytes() < header.payload.length) return;
+			in.readBytes(header.payload);
+			hasHeader = false;
+			if (header.id == 10108) {
+				header.decrypted = new byte[0];
 			} else {
-				logger.warn("Received undefined packet %s:\n%s", name, Hex.dump(header.decrypted));
+				crypto.decryptPacket(header);
 			}
-
-			return null;
+			ClientMessage message = processPacket();
+			if (message != null) out.add(message);
 		}
-
-		try {
-			DataStream dataStream = new DataStream(header.decrypted);
-			message.decode(dataStream);
-
-			// Messages must be decoded fully, because it can contain useful information, so we will try to decode it fully
-			if (!dataStream.eof()) {
-				logger.warn("Message %s is not decoded fully.\n\tDecoded part:\n%s\n\tOther part:\n%s",
-						message.getClass().getSimpleName(),
-						Hex.dump(header.decrypted, 0, dataStream.getOffset()),
-						Hex.dump(header.decrypted, dataStream.getOffset(), header.decrypted.length - dataStream.getOffset()));
-			}
-		} catch (Exception e) {
-			logger.error("Failed to decode packet %s, payload:\n%s", message.getClass().getSimpleName(),
-					Hex.dump(header.decrypted));
-			return null;
-		}
-
-		return message;
 	}
+
+    private ClientMessage processPacket() {
+        ClientMessage message = ClientMessageFactory.instance.create(header.id);
+
+        if (header.decrypted == null) {
+            logger.error("Failed to decrypt packet %d, encrypted payload:\n%s", header.id, Hex.dump(header.payload));
+            return null;
+        }
+
+        if (message == null) {
+            String name = null;
+            if (Messages.messagesMap.containsKey(header.id)) name = Messages.messagesMap.get(header.id);
+
+            if (name == null) {
+                logger.warn("Received unknown packet %d:\n%s", header.id, Hex.dump(header.decrypted));
+            } else {
+                logger.warn("Received undefined packet %s:\n%s", name, Hex.dump(header.decrypted));
+            }
+            return null;
+        }
+
+        try {
+            DataStream dataStream = new DataStream(header.decrypted);
+            message.decode(dataStream);
+
+            // Messages must be decoded fully, because they can contain useful information.
+            if (!dataStream.eof()) {
+                logger.warn(
+					"Message %s is not decoded fully.\n\tDecoded part:\n%s\n\tOther part:\n%s",
+					message.getClass().getSimpleName(),
+					Hex.dump(header.decrypted, 0, dataStream.getOffset()),
+					Hex.dump(
+						header.decrypted,
+						dataStream.getOffset(),
+						header.decrypted.length - dataStream.getOffset()
+					)
+                );
+            }
+        } catch (Exception e) {
+            logger.error("Failed to decode packet %s, payload:\n%s", message.getClass().getSimpleName(), Hex.dump(header.decrypted));
+            return null;
+        }
+
+        return message;
+    }
 }
